8f02290f4df9ddc8b43a05349706b98b544fe190
Herzogmoffo <herzogmoffo@gmail.com>
2023-11-06 15:36:53 +0100

    commit message

--- src/Home.js
+++ src/Home.js
@@ -43,8 +43,8 @@ function Home() {
 
             <div className="section section5">
                 <div className="section5-text">
-                    <h2>Ihr Partner für gelungene </h2><h2>Veranstaltungen</h2>
-                   <h2> Erfahren Sie, wie </h2><h2>All-Activity-Group </h2><h2>als Ihr Partner für </h2><h2>Gastronomie- und </h2> <h2>Einzelhandelsservices zum </h2> <h2> Erfolg Ihrer </h2> <h2>Veranstaltungen beiträgt.</h2>
+                    <h2>All-activity-group Ihr Partner für  </h2><h2> gelungene Veranstaltungen.</h2>
+                  
                 </div>
             </div>
 

b5ae7ffbe95a24243056f5f21983bc24093e5b8d
Herzogmoffo <herzogmoffo@gmail.com>
2023-11-09 10:55:12 +0100

    Premier commit

--- src/Services.js
+++ src/Services.js
@@ -14,8 +14,8 @@ function Service() {
             {/* Section 2 */}
             <div className="section rectangle02">
                 <div className="text-container">
-                    <h3 className="titre1">Unübertroffene Servicequalität</h3>
-                    <p className="texte">Unsere jahrelange Erfahrung im Catering-Bereich hat uns gezeigt, wie wichtig erstklassiger Service ist. Bei jeder Veranstaltung, sei es ein großes Bankett oder ein intimes Abendessen, setzen wir alles daran, höchste Standards in der Servicequalität zu erfüllen. Jedes Detail, von der Vorbereitung bis zur Präsentation, wird sorgfältig berücksichtigt, um unseren Kunden ein makelloses Erlebnis zu bieten.</p>
+                    <h3 className="titre1">Vollumfänglicher Service mit Eigenverantwortung</h3>
+                    <p className="texte">Wir möchten darauf hinweisen, dass unsere Dienstleistungen ausschließlich auf der Basis von Eigenverantwortung und Vollständigkeit der übernommenen Bereiche angeboten werden. Unser Ansatz besteht darin, komplette Servicebereiche zu managen, anstatt lediglich Personal zur Verfügung zu stellen.</p>
                 </div>
                 <div className="image-container">
                     <img src="image9.webp" alt="Service" />
@@ -25,16 +25,16 @@ function Service() {
             {/* Section 3 */}
             <div className="section white-section">
                 <div className="box">
-                    <h4 className="titre2">Unser Engagement für Exzellenz</h4>
-                    <p className="texte">Für uns ist jeder Event nicht nur eine weitere Aufgabe, sondern eine Gelegenheit, uns selbst zu übertreffen. Dieses Engagement für Exzellenz zeigt sich in jedem Aspekt unseres Geschäfts. Unsere Kunden sind unser wertvollstes Gut, und ihre Zufriedenheit ist der Maßstab für unseren Erfolg. Daher gehen wir bei jedem Auftrag die Extra-Meile, um sicherzustellen, dass ihre Erwartungen nicht nur erfüllt, sondern übertroffen werden.</p>
+                    <h4 className="titre2">Professionelle Bankett-Servicekräfte für Großveranstaltungen</h4>
+                    <p className="texte">Unsere erfahrenen Bankett-Servicekräfte sind spezialisiert auf die Bedienung und das Management von Großveranstaltungen. Bei einer kürzlich organisierten Firmenfeier mit 500 Gästen haben wir 20 top-geschulte Servicekräfte bereitgestellt. Diese Veranstaltung wurde von einem externen Anbieter organisiert und demonstrierte unsere Fähigkeit, effektiv in großem Umfang zu arbeiten.</p>
                 </div>
                 <div className="box">
-                    <h4 className="titre3">Bewährte Erfahrung</h4>
-                    <p className="texte">Seit unserer Gründung haben wir unzählige Veranstaltungen begleitet und uns einen Namen als eines der zuverlässigsten und renommiertesten Catering-Unternehmen gemacht. Unsere Erfahrung erstreckt sich über eine Vielzahl von Events, wodurch wir in der Lage sind, uns an verschiedene Bedürfnisse und Wünsche anzupassen und gleichzeitig unsere einzigartige Note beizubehalten.</p>
+                    <h4 className="titre3">Umfassender Auf- und Abbau des Buffets</h4>
+                    <p className="texte">Der Service beinhaltete den kompletten Aufbau sowie den Abbau des Buffets, das vom Veranstalter geliefert wurde. Unsere Servicekräfte haben sich um alle Aspekte des Buffet-Managements gekümmert, um eine ansprechende Präsentation und reibungslose Abläufe zu gewährleisten. Ebenso wurde das Geschirr, zur Verfügung gestellt vom Anbieter, nach der Veranstaltung von unseren Kräften abgeräumt und gespült..</p>
                 </div>
                 <div className="box">
-                    <h4 className="titre4">Höchster Professionalität</h4>
-                    <p className="texte">Unser Team besteht aus professionell ausgebildeten Kellnern und Catering-Experten, die sich durch ihre Leidenschaft für ihren Beruf und ihre Expertise auszeichnen. Diese Kombination aus Talent und Fachwissen ermöglicht es uns, ein Höchstmaß an Professionalität zu gewährleisten, sodass unsere Kunden sich entspannen und sicher sein können, dass ihre Gäste in besten Händen sind</p>
+                    <h4 className="titre4">Hervorragende Gästebetreuung</h4>
+                    <p className="texte">Ein wesentlicher Bestandteil unseres Services ist die Gewährleistung eines reibungslosen Veranstaltungsablaufs und die freundliche Bedienung aller Gäste. Unsere Servicekräfte sind darauf trainiert, auf die Bedürfnisse jedes Gastes einzugehen und einen exzellenten Service zu bieten, der den Erfolg Ihrer Veranstaltung sicherstellt.</p>
                 </div>
             </div>
 
